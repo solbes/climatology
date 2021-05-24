2ae77bb083ab551e2643452390f05df7b3b848da
Stratos Staboulis <stratos.staboulis@leanheat.com>
2021-05-25 00:24:02 +0300

    Add config file as a command line argument

--- api.py
+++ api.py
@@ -1,30 +1,19 @@
+"""Script for starting weather data Flask server
+
+"""
+
+import argparse
+import json
+import logging
+import os
+import requests
+
 import flask
 from flask import request, jsonify
-import logging
-import pandas as pd
-import numpy as np
-import xarray as xr
-import requests
-import json
 import matplotlib.pyplot as plt
-import os
-
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
-# load configurations
-with open('loader_config.json') as json_file:
-    cfg_all = json.load(json_file)
-
-# open all data files
-ds_disks = {key: {} for key in cfg_all.keys()}
-for region in cfg_all.keys():
-    cfg = cfg_all[region]
-    for var in cfg['variables']:
-        path = os.path.join(cfg['path'], region, var + '*.nc')
-        print(path)
-        ds_disks[region][var] = xr.open_mfdataset(path)
+import numpy as np
+import pandas as pd
+import xarray as xr
 
 
 def in_area(lat, lon, area):
@@ -45,127 +34,154 @@ def round_to_base(x, prec=2, base=.25):
     return round(base * round(float(x)/base), prec)
 
 
-@app.route('/', methods=['GET'])
-def home():
+def create_app(configs):
 
-    mon = pd.Timestamp.now().month
-    day = pd.Timestamp.now().day
+    # open all data files
+    ds_disks = {key: {} for key in configs}
+    for (region, config) in configs.items():
+        for var in config['variables']:
+            glob_path = os.path.join(config['path'], region, var + '*.nc')
+            print(glob_path)
+            ds_disks[region][var] = xr.open_mfdataset(glob_path)
 
-    req = 'http://127.0.0.1:5000/api/by_day?variable' \
-          '=2m_temperature&lat=61.75&lon=22.5' \
-          '&month={0}&day={1}&start=2016'.format(mon, day)
+    app = flask.Flask(__name__)
+    app.config["DEBUG"] = True
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-    res = requests.get(req)
+    @app.route('/', methods=['GET'])
+    def home():
 
-    ds = pd.DataFrame(json.loads(res.content))
-    ds = ds.set_index(pd.to_datetime(ds.time)).drop(columns='time')-273.15
+        mon = pd.Timestamp.now().month
+        day = pd.Timestamp.now().day
 
-    plt.figure()
-    plt.scatter(ds.index.hour, ds.temperature, c=ds.index.year, s=10)
-    plt.xlabel('hour')
-    plt.ylabel('2m temperature [C]')
-    plt.title('Todays temperature in Kankaanpää, color=year since 2016')
-    plt.grid(True)
-    plt.savefig('./static/home_fig.png', dpi=200)
+        req = 'http://127.0.0.1:5000/api/by_day?variable' \
+            '=2m_temperature&lat=61.75&lon=22.5' \
+            '&month={0}&day={1}&start=2016'.format(mon, day)
 
-    return "<h1>Climatology API</h1>" \
-           "<p>This site is a prototype API for historical weather data.</p>" \
-           "<figure><img src='/static/home_fig.png' style='width:40%'></figure>"
+        res = requests.get(req)
+
+        ds = pd.DataFrame(json.loads(res.content))
+        ds = ds.set_index(pd.to_datetime(ds.time)).drop(columns='time')-273.15
+
+        plt.figure()
+        plt.scatter(ds.index.hour, ds.temperature, c=ds.index.year, s=10)
+        plt.xlabel('hour')
+        plt.ylabel('2m temperature [C]')
+        plt.title('Todays temperature in Kankaanpää, color=year since 2016')
+        plt.grid(True)
+        plt.savefig('./static/home_fig.png', dpi=200)
+
+        return "<h1>Climatology API</h1>" \
+            "<p>This site is a prototype API for historical weather data.</p>" \
+            "<figure><img src='/static/home_fig.png' style='width:40%'></figure>"
+
+    @app.route('/api/by_range', methods=['GET'])
+    def api_by_range():
+
+        # round lat and lon to nearest 0.25
+        lat = round_to_base(float(request.args['lat']), prec=2, base=0.25)
+        lon = round_to_base(float(request.args['lon']), prec=2, base=0.25)
+        variable = request.args['variable']
+
+        # figure out the region to use
+        ok_region = region_from_coords(lat, lon, configs)
+        if not ok_region:
+            return "No suitable area found with given lat-lon."
+
+        ds_disk = ds_disks[ok_region][variable]
+
+        start = request.args['start'] if 'start' in request.args else '2020'
+        end = request.args['end'] if 'end' in request.args else pd.Timestamp.now()
+
+        temp_series = ds_disk.sel(
+            time=slice(start, end),
+            longitude=lon,
+            latitude=lat
+        )
+
+        # in some files you get a nasty extra dimension 'expver' in addition to
+        # time, see some discussion here:
+        # https://confluence.ecmwf.int/pages/viewpage.action?pageId=173385064
+        # this is a workaround to drop the extra dimension
+        if 'expver' in list(temp_series.indexes.keys()):
+            temp_series = temp_series.reduce(np.nansum, 'expver')
+
+        # drop lat and lon --> you have a dataframe with just one variable
+        temp_series = temp_series.to_dataframe().drop(columns=['latitude', 'longitude'])
+
+        out = {'time': list(temp_series.index.astype(str)),
+            'temperature': list(temp_series.values[:, 0].astype(float))}
+
+        return jsonify(out)
+
+    @app.route('/api/by_day', methods=['GET'])
+    def api_temperature_day():
+
+        print(request.args)
+
+        lat = round_to_base(float(request.args['lat']), prec=2, base=0.25)
+        lon = round_to_base(float(request.args['lon']), prec=2, base=0.25)
+        mon = int(request.args['month'])
+        day = int(request.args['day'])
+        variable = request.args['variable']
+
+        # figure out the region to use
+        ok_region = region_from_coords(lat, lon, configs)
+        if not ok_region:
+            return "No suitable area found with given lat-lon."
+
+        ds_disk = ds_disks[ok_region][variable]
+
+        start = int(request.args['start']) if 'start' in request.args else 1979
+        end = int(request.args['end']) if 'end' in request.args else \
+            pd.Timestamp.now().year
+
+        time = ds_disk.time.to_dataframe().index
+
+        ii_mon = time.month == mon
+        ii_day = time.day == day
+        ii_year = (time.year >= start) & (time.year <= end)
+
+        temp_series = ds_disk.sel(
+            time=(ii_mon & ii_day & ii_year), longitude=lon, latitude=lat,
+        )
+
+        # in some files you get a nasty extra dimension 'expver' in addition to
+        # time, see some discussion here:
+        # https://confluence.ecmwf.int/pages/viewpage.action?pageId=173385064
+        # this is a workaround to drop the extra dimension
+        if 'expver' in list(temp_series.indexes.keys()):
+            temp_series = temp_series.reduce(np.nansum, 'expver')
+
+        temp_series = temp_series.to_dataframe().drop(
+            columns=['latitude', 'longitude'])
+
+        out = {'time': list(temp_series.index.astype(str)),
+            'temperature': list(temp_series.values[:, 0].astype(float))}
+
+        return jsonify(out)
+
+    # No caching at all for API endpoints.
+    @app.after_request
+    def add_header(response):
+        response.headers['Cache-Control'] = 'public, max-age=0'
+        return response
+
+    return app
 
 
-@app.route('/api/by_range', methods=['GET'])
-def api_by_range():
+if __name__ == "__main__":
 
-    # round lat and lon to nearest 0.25
-    lat = round_to_base(float(request.args['lat']), prec=2, base=0.25)
-    lon = round_to_base(float(request.args['lon']), prec=2, base=0.25)
-    variable = request.args['variable']
+    parser = argparse.ArgumentParser(description="Data loader arguments")
+    parser.add_argument('-c', '--configs-file', help='path to configuration JSON file')
+    args = parser.parse_args()
 
-    # figure out the region to use
-    ok_region = region_from_coords(lat, lon, cfg_all)
-    if not ok_region:
-        return "No suitable area found with given lat-lon."
+    # load configurations
+    with open(args.configs_file) as f:
+        configs = json.load(f)
 
-    ds_disk = ds_disks[ok_region][variable]
+    app = create_app(configs)
 
-    start = request.args['start'] if 'start' in request.args else '2020'
-    end = request.args['end'] if 'end' in request.args else pd.Timestamp.now()
-
-    temp_series = ds_disk.sel(
-        time=slice(start, end),
-        longitude=lon,
-        latitude=lat
-    )
-
-    # in some files you get a nasty extra dimension 'expver' in addition to
-    # time, see some discussion here:
-    # https://confluence.ecmwf.int/pages/viewpage.action?pageId=173385064
-    # this is a workaround to drop the extra dimension
-    if 'expver' in list(temp_series.indexes.keys()):
-        temp_series = temp_series.reduce(np.nansum, 'expver')
-
-    # drop lat and lon --> you have a dataframe with just one variable
-    temp_series = temp_series.to_dataframe().drop(columns=['latitude', 'longitude'])
-
-    out = {'time': list(temp_series.index.astype(str)),
-           'temperature': list(temp_series.values[:, 0].astype(float))}
-
-    return jsonify(out)
-
-
-@app.route('/api/by_day', methods=['GET'])
-def api_temperature_day():
-
-    print(request.args)
-
-    lat = round_to_base(float(request.args['lat']), prec=2, base=0.25)
-    lon = round_to_base(float(request.args['lon']), prec=2, base=0.25)
-    mon = int(request.args['month'])
-    day = int(request.args['day'])
-    variable = request.args['variable']
-
-    # figure out the region to use
-    ok_region = region_from_coords(lat, lon, cfg_all)
-    if not ok_region:
-        return "No suitable area found with given lat-lon."
-
-    ds_disk = ds_disks[ok_region][variable]
-
-    start = int(request.args['start']) if 'start' in request.args else 1979
-    end = int(request.args['end']) if 'end' in request.args else \
-        pd.Timestamp.now().year
-
-    time = ds_disk.time.to_dataframe().index
-
-    ii_mon = time.month == mon
-    ii_day = time.day == day
-    ii_year = (time.year >= start) & (time.year <= end)
-
-    temp_series = ds_disk.sel(
-        time=(ii_mon & ii_day & ii_year), longitude=lon, latitude=lat,
-    )
-
-    # in some files you get a nasty extra dimension 'expver' in addition to
-    # time, see some discussion here:
-    # https://confluence.ecmwf.int/pages/viewpage.action?pageId=173385064
-    # this is a workaround to drop the extra dimension
-    if 'expver' in list(temp_series.indexes.keys()):
-        temp_series = temp_series.reduce(np.nansum, 'expver')
-
-    temp_series = temp_series.to_dataframe().drop(
-        columns=['latitude', 'longitude'])
-
-    out = {'time': list(temp_series.index.astype(str)),
-           'temperature': list(temp_series.values[:, 0].astype(float))}
-
-    return jsonify(out)
-
-
-# No caching at all for API endpoints.
-@app.after_request
-def add_header(response):
-    response.headers['Cache-Control'] = 'public, max-age=0'
-    return response
-
-
-app.run()
+    # -- Run server -- #
+    app.run()  #########
+    # -----------------#
--- load_data.py
+++ load_data.py
@@ -1,12 +1,25 @@
-import pandas as pd
-import json
-import cdsapi
-import os
-import xarray as xr
+"""Script for loading weather data to local disk
+
+"""
+
+import argparse
 import glob
+import json
+import os
+from typing import List
+
+import cdsapi
+import pandas as pd
+import xarray as xr
 
 
-def load_month(variable, y, m, area, path):
+def load_month(
+        variable: str,
+        year: int,
+        month: int,
+        area: List[float],
+        path: str
+):
 
     c = cdsapi.Client()
 
@@ -15,8 +28,8 @@ def load_month(variable, y, m, area, path):
         {
             'product_type': 'reanalysis',
             'variable': variable,
-            'year': int(year),
-            'month': int(month),
+            'year': year,
+            'month': month,
             'day': [
                 '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
                 '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
@@ -32,48 +45,69 @@ def load_month(variable, y, m, area, path):
             'format': 'netcdf',
             'area': area,
         },
-        os.path.join(path, '{0}_{1}-{2}.nc'.format(variable, y, m))
+        os.path.join(path, f'{variable}_{year}-{month}.nc')
     )
 
 
-with open('loader_config.json') as json_file:
-    cfg_all = json.load(json_file)
+def main(configs: dict) -> None:
 
-regions = cfg_all.keys()
+    regions = configs.keys()
 
-for region in regions:
+    for (region, config) in configs.items():
 
-    cfg = cfg_all[region]
-    variables = cfg['variables']
+        variables = config['variables']
+        area = config['area']
+        path = config['path']
+        start = config['start']
 
-    for var in variables:
+        for variable in variables:
 
-        # calculate expected number of hours per month
-        # TODO: this is a dumb way, could just use pd.Timestamp.daysinmonth
-        hours = pd.date_range(start=str(cfg['start']), end=pd.Timestamp.now(),
-                              freq='h')
-        hours_str = [t.strftime('%Y-%m') for t in hours]
-        expected_counts = pd.Series(hours_str).groupby(hours_str).count()
+            # calculate expected number of hours per month
+            # TODO: this is a dumb way, could just use pd.Timestamp.daysinmonth
+            hours = pd.date_range(
+                start=str(start),
+                end=pd.Timestamp.now(),
+                freq='h'
+            )
+            hours_str = [t.strftime('%Y-%m') for t in hours]
+            expected_counts = pd.Series(hours_str).groupby(hours_str).count()
 
-        # check how many hours per month exist in data
-        # TODO: what if no files exist?
-        path = os.path.join(cfg['path'], region, var + '*')
-        if glob.glob(path):
-            ds_disk = xr.open_mfdataset(path)
-            existing_times = pd.to_datetime(ds_disk.time.to_series())
-            existing_counts = existing_times.groupby(
-                [t.strftime('%Y-%m') for t in existing_times]
-            ).count().reindex(expected_counts.index).fillna(0)
-        else:
-            existing_counts = pd.Series(index=expected_counts.index, data=0)
+            # check how many hours per month exist in data
+            # TODO: what if no files exist?
+            glob_path = os.path.join(path, region, variable + '*')
+            if glob.glob(glob_path):
+                ds_disk = xr.open_mfdataset(glob_path)
+                existing_times = pd.to_datetime(ds_disk.time.to_series())
+                existing_counts = existing_times.groupby(
+                    [t.strftime('%Y-%m') for t in existing_times]
+                ).count().reindex(expected_counts.index).fillna(0)
+            else:
+                existing_counts = pd.Series(index=expected_counts.index, data=0)
 
-        # figure out the months for which data is incomplete
-        incomplete = existing_counts.index[existing_counts < expected_counts]
+            # figure out the months for which data is incomplete
+            incomplete = existing_counts.index[existing_counts < expected_counts]
 
-        years = [s.split('-')[0] for s in incomplete]
-        months = [s.split('-')[1] for s in incomplete]
+            years = [s.split('-')[0] for s in incomplete]
+            months = [s.split('-')[1] for s in incomplete]
 
-        # load the incomplete months
-        for year, month in zip(years, months):
-            load_month(var, year, month,
-                       cfg['area'], os.path.join(cfg['path'], region))
+            # load the incomplete months
+            for year, month in zip(years, months):
+                load_month(
+                    variable,
+                    int(year),
+                    int(month),
+                    area,
+                    os.path.join(path, region)
+                )
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Data loader arguments")
+    parser.add_argument('-c', '--configs-file', help='path to configuration JSON file')
+    args = parser.parse_args()
+
+    with open(args.configs_file) as f:
+        configs = json.load(f)
+
+    main(configs)
